047ada5c4c02b3f78a7f584f6d95908ae7c8c4e5
shankargunti <40358462+shankargunti@users.noreply.github.com>
2019-05-01 19:44:18 -0400

    new files
    new files

---
+++ Pages/Contact.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ApplicationInsights.Pages
+{
+    public class ContactModel : PageModel
+    {
+        public string Message { get; set; }
+
+        public void OnGet()
+        {
+            Message = "Your contact page.";
+            throw new System.IndexOutOfRangeException("Message : ApplicationInsights : New Exception");
+        }
+    }
+}
